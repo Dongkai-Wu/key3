36024882362c8d1148066c4e84e897c26070632b
Dongkai Wu <wudongkai777@gmail.com>
2022-08-18 14:30:51 -0400

    2 steps

--- Untitled.R
+++ Untitled.R
@@ -1,12 +1,33 @@
-a=c(1,2,3)
-b=c(-2,0.5,2)
-t(a) %*% b
+myName <- "Dongkai Wu"
 
-crossprod(a,b)
+#2
+tmp <- seq(3,6, by=0.1)
+v2 <- exp(tmp)*cos(tmp)
+plot(ans)
 
-a=1:3
-b=1:2
-a %*% t( b )
-a=as.vector(c(1,2,3))
-a
-a%*%a
+#3,(a)
+S1 <- seq(3,36,3)
+S2 <- seq(1,34,3)
+v3a <- (0.1^S1)*(0.2^S2)
+
+#3(b)
+S3 <- seq(2,25,1)
+S4 <- seq(2,25,1)
+v3b <- c(2,(2^S3)/S4)
+
+
+#4,(a)
+S5 <- seq(10,100,1)
+v4a <- (S5^3)+4*(S5^2)
+ans
+
+#4,(b)
+S6 <- seq(1,25,1)
+v4b <- (2^(S6)/S6)+(3^(S6)/(S6)^2)
+ans
+
+#5(a)
+S7 <- seq(1,30,1)
+v5a <- paste("label",S7)
+#5(b)
+v5b <- paste("label",S7)
