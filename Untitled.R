6bc1e083f220b38074ad8131217e0f6f19f83f31
Dongkai Wu <wudongkai777@gmail.com>
2022-08-18 14:33:22 -0400

    final

--- Untitled.R
+++ Untitled.R
@@ -3,7 +3,6 @@ myName <- "Dongkai Wu"
 #2
 tmp <- seq(3,6, by=0.1)
 v2 <- exp(tmp)*cos(tmp)
-plot(ans)
 
 #3,(a)
 S1 <- seq(3,36,3)
@@ -19,12 +18,10 @@ v3b <- c(2,(2^S3)/S4)
 #4,(a)
 S5 <- seq(10,100,1)
 v4a <- (S5^3)+4*(S5^2)
-ans
 
 #4,(b)
 S6 <- seq(1,25,1)
 v4b <- (2^(S6)/S6)+(3^(S6)/(S6)^2)
-ans
 
 #5(a)
 S7 <- seq(1,30,1)
